8cd95e79824d2b60f35bac471b2306e3c1cd09be
Maksym Chukhno <chukhno.maksym@gmail.com>
2024-02-23 09:13:12 +0200

    update index.html

---
+++ JS/slider.js
@@ -0,0 +1,29 @@
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  slidesPerView: 5,
+  spaceBetween: 10,
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 40,
+    },
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
