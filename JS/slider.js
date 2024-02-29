d545fb196908ad2faa387bdc72b7ad4c440f1ef3
Maksym Chukhno <chukhno.maksym@gmail.com>
2024-02-29 17:19:23 +0200

    add slider

--- JS/slider.js
+++ JS/slider.js
@@ -1,22 +1,16 @@
 const swiper = new Swiper(".swiper", {
   loop: true,
-  slidesPerView: 5,
+  slidesPerView: 1,
   spaceBetween: 10,
   breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20,
+    720: {
+      slidesPerView: 1,
     },
-    // when window width is >= 480px
-    480: {
+    1000: {
       slidesPerView: 2,
-      spaceBetween: 30,
     },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 2,
-      spaceBetween: 40,
+    1200: {
+      slidesPerView: 3,
     },
   },
   pagination: {
